1dc142e698127a16ed631419b17c532990d90c65
ShinYoung-Kim <lukey0515@naver.com>
2023-12-27 01:19:01 +0900

    feat: postmessage 코드 추가

--- src/Map.tsx
+++ src/Map.tsx
@@ -2,25 +2,35 @@ import { useEffect, useState } from "react";
 
 const { kakao } = window;
 
+declare global {
+    interface Document {
+        ReactNativeWebView: {
+            postMessage: (message: string) => void;
+        }
+    }
+
+    interface Window {
+        ReactNativeWebView: {
+            postMessage: (message: string) => void;
+        }
+    }
+}
+
 function Map() {
     const [position, setPosition] = useState({ latitude: 37.5665, longitude: 126.9780 });
     const listener = (event: MessageEvent) => {
-        if (event.origin !== 'app') {
-            setPosition({ latitude: 0, longitude: 0 });
-            return;
-        } else {
-            setPosition({ latitude: 10, longitude: 10 });
+        const data = typeof event.data !== 'object' ? JSON.parse(event.data) : event.data;
+        if (data.type === 'map') {
+            const positionByApp = JSON.parse(event.data);
+            setPosition(positionByApp);
+            loadMap(positionByApp);
         }
-
-        const position = event.data;
-        setPosition(position);
-        loadMap(position);
     }
-
-    window.addEventListener('message', listener);
 
     useEffect(() => {
         loadMap(position);
+        document.addEventListener('message', listener as EventListener);
+        window.addEventListener('message', listener as EventListener);
     }, [])
 
     function loadMap({ latitude, longitude }: { latitude: number, longitude: number }) {
