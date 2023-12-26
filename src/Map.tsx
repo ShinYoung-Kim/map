13b519e75ee19e043adb813e51b29505a258a0a6
ShinYoung-Kim <lukey0515@naver.com>
2023-12-26 10:45:54 +0900

    feat: load map 함수 구현

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-// const { kakao } = window;
+const { kakao } = window;
 
 function Map() {
     function initWebView() {
@@ -13,26 +13,26 @@ function Map() {
             const position = event.data;
             console.log('in');
             console.log(position);
-            // loadMap(position);
+            loadMap(position);
         }
 
         window.addEventListener('message', listener);
     }
 
-    // function loadMap() {
-    //     const container = document.getElementById('map');
-    //     const options = {
-    //         // center: new kakao.maps.LatLng(latitude, longitude),
-    //         center: new kakao.maps.LatLng(37.506502, 127.053617),
-    //         level: 3
-    //     };
-    //     const map = new kakao.maps.Map(container!, options);
-    //     return map;
-    // }
+    function loadMap({ latitude, longitude }: { latitude: number, longitude: number }) {
+        const container = document.getElementById('map');
+        const options = {
+            center: new kakao.maps.LatLng(latitude, longitude),
+            // center: new kakao.maps.LatLng(37.506502, 127.053617),
+            level: 3
+        };
+        const map = new kakao.maps.Map(container!, options);
+        return map;
+    }
 
     useEffect(() => {
         initWebView();
-        // loadMap();
+        loadMap();
     }, [])
 
     return (
