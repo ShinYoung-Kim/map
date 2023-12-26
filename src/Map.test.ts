ec541163b061f78a3a3cd4d2e743616db3b32802
ShinYoung-Kim <lukey0515@naver.com>
2023-12-26 10:45:12 +0900

    feat: message listener 테스트

---
+++ src/Map.test.ts
@@ -0,0 +1,39 @@
+import Map from "./Map";
+import { describe, expect, it, vi } from "vitest";
+import { renderHook } from "@testing-library/react";
+
+describe("Map 컴포넌트 테스트", () => {
+	it("postMessage로 position을 받으면, console에 찍힌다.", () => {
+		const map = renderHook(() => Map());
+		map;
+		window.postMessage(
+			{
+				type: "position",
+				payload: { lat: 37.123, lng: 127.123 },
+			},
+			"app"
+		);
+		expect(console.log).toHaveBeenCalledWith("in");
+		expect(console.log).toHaveBeenCalledWith({
+			lat: 37.123,
+			lng: 127.123,
+		});
+	});
+
+	it("message의 origin이 app이 아니면, console에 찍히지 않는다.", () => {
+		const map = renderHook(() => Map());
+		map;
+		window.postMessage(
+			{
+				type: "position",
+				payload: { lat: 37.123, lng: 127.123 },
+			},
+			"not app"
+		);
+		expect(console.log).not.toHaveBeenCalledWith("in");
+		expect(console.log).not.toHaveBeenCalledWith({
+			lat: 37.123,
+			lng: 127.123,
+		});
+	});
+});
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,20 +1,38 @@
 import { useEffect } from "react";
 
-const { kakao } = window;
+// const { kakao } = window;
 
 function Map() {
-    function loadMap() {
-        const container = document.getElementById('map');
-        const options = {
-            center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-            level: 3
-        };
-        const map = new kakao.maps.Map(container!, options);
-        return map;
+    function initWebView() {
+        const listener = (event: MessageEvent) => {
+            // const position = JSON.parse(event.data);
+            if (event.origin !== 'app') {
+                return;
+            }
+
+            const position = event.data;
+            console.log('in');
+            console.log(position);
+            // loadMap(position);
+        }
+
+        window.addEventListener('message', listener);
     }
 
+    // function loadMap() {
+    //     const container = document.getElementById('map');
+    //     const options = {
+    //         // center: new kakao.maps.LatLng(latitude, longitude),
+    //         center: new kakao.maps.LatLng(37.506502, 127.053617),
+    //         level: 3
+    //     };
+    //     const map = new kakao.maps.Map(container!, options);
+    //     return map;
+    // }
+
     useEffect(() => {
-        loadMap();
+        initWebView();
+        // loadMap();
     }, [])
 
     return (
