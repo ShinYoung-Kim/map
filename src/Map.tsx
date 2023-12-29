2e1589baf71ae2af477c47bf436be52dbe3959eb
ShinYoung-Kim <lukey0515@naver.com>
2023-12-29 16:11:52 +0900

    feat: 지도 클릭 시 마커 추가 구현

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import BookmarkButton from "./component/BookmarkButton";
+import SaveButton from "./component/SaveButton";
 
 const { kakao } = window;
 
@@ -18,6 +20,7 @@ declare global {
 
 function Map() {
     const [position, setPosition] = useState({ latitude: 37.5665, longitude: 126.9780 });
+    const markerRef = useRef<kakao.maps.Marker>();
     const listener = (event: MessageEvent) => {
         const data = typeof event.data !== 'object' ? JSON.parse(event.data) : event.data;
         if (data.type === 'map') {
@@ -28,9 +31,10 @@ function Map() {
     }
 
     useEffect(() => {
-        loadMap(position);
+        const map = loadMap(position);
         document.addEventListener('message', listener as EventListener);
         window.addEventListener('message', listener as EventListener);
+        addMarker(map);
     }, [])
 
     function loadMap({ latitude, longitude }: { latitude: number, longitude: number }) {
@@ -46,10 +50,33 @@ function Map() {
             position: new kakao.maps.LatLng(latitude, longitude),
         });
         marker.setMap(map);
+
+        return map;
+    }
+
+    function addMarker(map: kakao.maps.Map) {
+        kakao.maps.event.addListener(map, 'click', function (mouseEvent: kakao.maps.event.MouseEvent) {
+            const latlng = mouseEvent.latLng;
+            const marker = new kakao.maps.Marker({
+                position: latlng,
+            });
+
+            if (!markerRef.current) {
+                markerRef.current = marker;
+                marker.setMap(map);
+                return;
+            }
+
+            markerRef.current.setPosition(latlng);
+        });
     }
 
     return (
-        <div id="map" />
+        <div>
+            <div id="map" />
+            <BookmarkButton />
+            <SaveButton />
+        </div>
     );
 }
 
